f2fc4f3547c48812a58e990f319ea48080fb1901
Grant Kazan <grantkazan@gmail.com>
2018-09-06 10:37:40 -0500

    display all tickets on orders page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Admin</router-link> |
-      <router-link to="/about">User</router-link>
+      <router-link to="/about">User</router-link> | 
+      <router-link to="/orders">View Orders</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Orders.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>{{message}}</h1>
+    <h2>All Orders</h2>
+    <div v-for="ticket in tickets">
+      <p>Movie: {{ticket.movie}}</p>
+      <p>Time: {{ticket.time}}</p>
+      <p>Theater: {{ticket.theater}}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+var axios = require("axios");
+
+export default {
+  data: function() {
+    return {
+      tickets: [],
+      message: "View Orders"
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/tickets").then(
+      function(response) {
+        console.log(response);
+        this.tickets = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {
+  },
+  computed: {}
+};
+</script>
